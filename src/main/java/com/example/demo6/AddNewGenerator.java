25d9450acdec114407cca5d66268dcd271d0087e
Kisaja <87978775+Kisaja@users.noreply.github.com>
2021-09-09 00:54:22 +0530

    2sceene

--- src/main/java/com/example/demo6/AddNewGenerator.java
+++ src/main/java/com/example/demo6/AddNewGenerator.java
@@ -1,4 +1,8 @@
 package com.example.demo6;
 
+import javafx.event.ActionEvent;
+
 public class AddNewGenerator {
+    public void Back(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/com/example/demo6/GeneratorMainPage.java
+++ src/main/java/com/example/demo6/GeneratorMainPage.java
@@ -4,13 +4,40 @@
 package com.example.demo6;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GeneratorMainPage {
+//    //declare the button
+//    @FXML
+//    private Button btn_cr;
+//
     //
     public void Search(ActionEvent actionEvent) {
     }
 
     public void Add_new_generator(ActionEvent actionEvent) {
+//        btn_cr.getScene().getWindow().hide();
+//        Stage signup = new Stage();
+//        Parent root = null;
+//        try {
+//            root = FXMLLoader.load(getClass().getResource("GeneratorMainPage.fxml"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        Scene scene = new Scene(root);
+//        scene.getStylesheets().add("stylesheet/main.css");
+//        scene.getStylesheets().add("stylesheet/login.css");
+//        scene.getStylesheets().add("stylesheet/GeneratorA.css");
+//        signup.setScene(scene);
+//        signup.show();
+//        signup.setResizable(false);
     }
 
     public void Back(ActionEvent actionEvent) {
