e64ab8de6fe00a3ab0224361daa3f2d30da47af3
Malith Senanayake <senanayakamalith@gmail.com>
2021-09-09 22:25:06 +0530

    employeeAttendanceCheck

---
+++ src/main/java/com/example/demo6/EmployeeAttendanceCheck.java
@@ -0,0 +1,41 @@
+package com.example.demo6;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class EmployeeAttendanceCheck {
+
+    @FXML
+    private DatePicker inputdate;
+
+    @FXML
+    private Button checkattendancebutton;
+
+    @FXML
+    private TextField inputnicno;
+
+    @FXML
+    private Button backbutton;
+
+    @FXML
+    private Button mainmenu;
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkattendance(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mainmenu(ActionEvent event) {
+
+    }
+
+}
