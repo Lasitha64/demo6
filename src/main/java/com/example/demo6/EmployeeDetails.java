4c0f3135371cbbef011f85183fd7d92826fa3b0e
Malith Senanayake <senanayakamalith@gmail.com>
2021-09-09 16:17:55 +0530

    Employee Details

---
+++ src/main/java/com/example/demo6/EmployeeDetails.java
@@ -0,0 +1,50 @@
+package com.example.demo6;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class EmployeeDetails {
+
+    @FXML
+    private Button addnewemployeebutton;
+
+    @FXML
+    private Button searchemployee;
+
+    @FXML
+    private Button employeeattendance;
+
+    @FXML
+    private ImageView backbutton;
+
+    @FXML
+    private Button mainmenu;
+
+    @FXML
+    void addnewemployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void employeeattendance(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mainmenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchemployee(ActionEvent event) {
+
+    }
+
+}
