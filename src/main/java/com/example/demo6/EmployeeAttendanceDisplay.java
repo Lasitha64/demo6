0db17dc41cf601d9701affe3332f1130491785fe
Malith Senanayake <senanayakamalith@gmail.com>
2021-09-09 22:38:51 +0530

    Employee Attendance Sheet

---
+++ src/main/java/com/example/demo6/EmployeeAttendanceDisplay.java
@@ -0,0 +1,39 @@
+package com.example.demo6;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class EmployeeAttendanceDisplay {
+
+    @FXML
+    private TextField inputname;
+
+    @FXML
+    private TextField inputusername;
+
+    @FXML
+    private TextField inputnic;
+
+    @FXML
+    private TextField inputmobile;
+
+    @FXML
+    private Button backbutton;
+
+    @FXML
+    private Button mainmenu;
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mainmenu(ActionEvent event) {
+
+    }
+
+}
+
