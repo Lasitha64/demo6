1ed3f580118b0c7348cd1634542949d19fbf5027
Malith Senanayake <senanayakamalith@gmail.com>
2021-09-13 01:18:11 +0530

    Admin Create Done

--- src/main/java/com/example/demo6/AdminSignUp.java
+++ src/main/java/com/example/demo6/AdminSignUp.java
@@ -1,5 +1,8 @@
 package com.example.demo6;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,6 +12,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import java.io.IOException;
 
@@ -41,6 +46,20 @@ public class AdminSignUp {
     @FXML
     private TextField inputpassword;
 
+    private MongoClient database;
+
+    MongoCollection<Document> AdminCollection;
+
+    @FXML
+
+    public void initialize(){
+        //initialize database connection
+        Database databaseController = new Database();
+        MongoDatabase database = databaseController.connectToDB("HerathCMD");
+
+        // get collection
+        AdminCollection = database.getCollection("Admin");
+    }
     @FXML
     void back(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
@@ -50,13 +69,43 @@ public class AdminSignUp {
         stage.setScene(scene);
         stage.show();
 
+
     }
 
     @FXML
     void createaccount(ActionEvent event) throws IOException {
 
+        Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("stylesheet/main.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+
+        try {
 
 
+            String inputnameText = inputname.getText(), inputusenameText = inputusename.getText(), inputemailText = inputemail.getText(), inputnicText = inputnic.getText(), inputmobileText = inputmobile.getText(), inputpasswordText = inputpassword.getText();
+            insertAdmin(AdminCollection, inputnameText, inputusenameText, inputemailText, inputnicText, inputmobileText, inputpasswordText);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void insertAdmin(MongoCollection<Document> adminCollection, String inputnameText, String inputusenameText, String inputemailText, String inputnicText, String inputmobileText, String inputpasswordText) {
+
+        Document admin = new Document("_id", new ObjectId())
+                .append("Name", inputnameText)
+                .append("Username", inputusenameText)
+                .append("Email", inputemailText)
+                .append("NIC No", inputnicText)
+                .append("Mobile No", inputmobileText)
+                .append("Password", inputpasswordText);
+        adminCollection.insertOne(admin);
+        System.out.println("Connection S3");
     }
 
     @FXML
@@ -71,4 +120,3 @@ public class AdminSignUp {
     }
 
 }
-
