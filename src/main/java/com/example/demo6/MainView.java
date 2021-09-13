54bb5c762e02d33660465e21fec933bc435fa6be
BlackBull3t <nimithmithsara12345@gmail.com>
2021-09-13 09:39:55 +0530

    aa

--- src/main/java/com/example/demo6/MainView.java
+++ src/main/java/com/example/demo6/MainView.java
@@ -69,7 +69,13 @@ public class MainView {
     }
 
     @FXML
-    void excavatoraction(ActionEvent event) {
+    void excavatoraction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Excav-main.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("stylesheet/Excav-main.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
 
     }
 
