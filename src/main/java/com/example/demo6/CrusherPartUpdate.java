ef500e7c96b95d28ffd1afb95769c83bd5434b66
Lasitha64 <68686464+Lasitha64@users.noreply.github.com>
2021-09-14 08:24:11 +0530

    Mongodb add

--- src/main/java/com/example/demo6/CrusherPartUpdate.java
+++ src/main/java/com/example/demo6/CrusherPartUpdate.java
@@ -1,8 +1,17 @@
 package com.example.demo6;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -11,25 +20,39 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
 import org.bson.Document;
+import javafx.scene.control.cell.TextFieldTableCell;
 import org.bson.conversions.Bson;
-import org.bson.types.ObjectId;
-import static com.mongodb.client.model.Filters.and;
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Updates.*;
+
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import com.example.demo6.Database;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.set;
 
 public class CrusherPartUpdate implements Initializable{
 
     private Stage stage;
     private Scene scene;
+    private int pos;
+    private String cruid;
+    private String cruname;
+    private String cruquan;
+    private String crupri;
+    private String crudate;
+    MongoClient database;
+    MongoCollection<Document> crusherCollection;
+    MongoCursor<Document> cursor = crusherCollection.find().iterator();
+
 
     @FXML
     private TableView<Crusher> CrusherParts;
@@ -65,7 +88,7 @@ public class CrusherPartUpdate implements Initializable{
     private TextField tf_date1;
 
     @FXML
-    private DatePicker tf_date;
+    private TextField tf_date;
 
     @FXML
     private Button btn_update;
@@ -78,70 +101,85 @@ public class CrusherPartUpdate implements Initializable{
 
     private AlertBox ab;
 
+    public ObservableList<Crusher> list;
+
+
+
+    public List attend = new ArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        showCrusher();
-    }
-
-    public Connection getConnection(){
-        Connection conn;
-        try{
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/crusherdb","root","test");
-            return conn;
-
-        }catch(Exception ex){
-            System.out.println("Error :"+ex.getMessage());
-            return null;
-        }
-    }
-
-    private void executeQuery(String query){
-        Connection conn = getConnection();
-        Statement st;
-        try{
-            st = conn.createStatement();
-            st.executeUpdate(query);
-        }catch(Exception ex){
-            ex.printStackTrace();
-        }
-
+        //    showCrusher();
+      //  CrusherParts.setEditable(true);
+        //initialize database connection
+        Database databaseController = new Database();
+        MongoDatabase database = databaseController.connectToDB("HerathCMD");
+        // get collection
+        crusherCollection = database.getCollection("Metal Crusher");
 
     }
 
-    public ObservableList<Crusher> getCrusherList(){
-        ObservableList<Crusher> bookList = FXCollections.observableArrayList();
-        Connection conn = getConnection();
-        String query = "select * from parts";
-        Statement st;
-        ResultSet rs;
 
-        try{
-            st = conn.createStatement();
-            rs = st.executeQuery(query);
-            Crusher cru;
-            while(rs.next()){
-                cru = new Crusher(rs.getString("ID"),rs.getString("Name"),rs.getInt("Quantity"),rs.getDouble("Price"),rs.getString("Date"));
-                bookList.add(cru);
-            }
-        }catch(Exception ex){
-            ex.printStackTrace();
-        }
-        return bookList;
-    }
 
-    public void showCrusher(){
-        ObservableList<Crusher>  list = getCrusherList();
-
-        cid.setCellValueFactory(new PropertyValueFactory<Crusher, String>("id"));
-        cname.setCellValueFactory(new PropertyValueFactory<Crusher, String>("name"));
-        cquan.setCellValueFactory(new PropertyValueFactory<Crusher, Integer>("quantity"));
-        cprice.setCellValueFactory(new PropertyValueFactory<Crusher, Double>("price"));
-        cdate.setCellValueFactory(new PropertyValueFactory<Crusher, String>("date"));
-
-        CrusherParts.setItems(list);
-
-    }
+//    public Connection getConnection(){
+//        Connection conn;
+//        try{
+//            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/crusherdb","root","test");
+//            return conn;
+//
+//        }catch(Exception ex){
+//            System.out.println("Error :"+ex.getMessage());
+//            return null;
+//        }
+//    }
+//
+//    private void executeQuery(String query){
+//        Connection conn = getConnection();
+//        Statement st;
+//        try{
+//            st = conn.createStatement();
+//            st.executeUpdate(query);
+//        }catch(Exception ex){
+//            ex.printStackTrace();
+//        }
+//
+//
+//    }
+//
+//    public ObservableList<Crusher> getCrusherList(){
+//        ObservableList<Crusher> bookList = FXCollections.observableArrayList();
+//        Connection conn = getConnection();
+//        String query = "select * from parts";
+//        Statement st;
+//        ResultSet rs;
+//
+//        try{
+//            st = conn.createStatement();
+//            rs = st.executeQuery(query);
+//            Crusher cru;
+//            while(rs.next()){
+//                cru = new Crusher(rs.getString("ID"),rs.getString("Name"),rs.getInt("Quantity"),rs.getDouble("Price"),rs.getString("Date"));
+//                bookList.add(cru);
+//            }
+//        }catch(Exception ex){
+//            ex.printStackTrace();
+//        }
+//        return bookList;
+//    }
+//
+//    public void showCrusher(){
+//        ObservableList<Crusher>  list = getCrusherList();
+//
+//        cid.setCellValueFactory(new PropertyValueFactory<Crusher, String>("id"));
+//        cname.setCellValueFactory(new PropertyValueFactory<Crusher, String>("name"));
+//        cquan.setCellValueFactory(new PropertyValueFactory<Crusher, Integer>("quantity"));
+//        cprice.setCellValueFactory(new PropertyValueFactory<Crusher, Double>("price"));
+//        cdate.setCellValueFactory(new PropertyValueFactory<Crusher, String>("date"));
+//
+//        CrusherParts.setItems(list);
+//
+//    }
 
     @FXML
     public void Back(ActionEvent event) throws IOException {
@@ -155,9 +193,13 @@ public class CrusherPartUpdate implements Initializable{
 
     @FXML
     void Delete(ActionEvent event) {
-        String query = "DELETE FROM parts WHERE id ='" + tf_id.getText() + "'";
-        executeQuery(query);
-        showCrusher();
+        String Stock_ID = tf_id.getText();
+        Bson filter = eq("Stock_ID", Stock_ID);
+        DeleteResult result = crusherCollection.deleteOne(filter);
+        System.out.println(result);
+//        String query = "DELETE FROM parts WHERE id ='" + tf_id.getText() + "'";
+//        executeQuery(query);
+//        showCrusher();
     }
 
     @FXML
@@ -172,9 +214,35 @@ public class CrusherPartUpdate implements Initializable{
             ab.display("Error","Price needs to be a double (ex: 200.90)");
         }
         else{
-            String query = "UPDATE parts SET Name = '"+tf_name.getText()+"' ,Quantity = "+tf_quan.getText()+",Price= "+tf_price.getText()+" ,Date= '"+tf_date1.getText()+"' WHERE ID ='" + tf_id.getText() + "'";
-            executeQuery(query);
-            showCrusher();
+//            String query = "UPDATE parts SET Name = '"+tf_name.getText()+"' ,Quantity = "+tf_quan.getText()+",Price= "+tf_price.getText()+" ,Date= '"+tf_date1.getText()+"' WHERE ID ='" + tf_id.getText() + "'";
+//            executeQuery(query);
+//            showCrusher();
+            String Stock_ID = tf_id.getText(),
+                    NameText = tf_name.getText() ,
+                    QuantityText = tf_quan.getText() ,
+                    PriceText = tf_price.getText(),
+                    DateText = tf_date.getText();
+
+            System.out.println(Stock_ID + NameText + QuantityText + PriceText + DateText);
+
+            Bson filter = eq("Stock_ID", Stock_ID);
+
+            Bson updateName = set("Name", NameText);
+            Bson updateQuan = set("Quantity", QuantityText);
+            Bson updatePrice = set("price", PriceText);
+            Bson updateDate = set("Date", DateText);
+
+            List<Bson> updatePredicates = new ArrayList<Bson>();
+            updatePredicates.add(updateName);
+            updatePredicates.add(updateQuan);
+            updatePredicates.add(updatePrice);
+            updatePredicates.add(updateDate);
+
+            FindOneAndUpdateOptions optionAfter = new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER);
+            Document newVersion = crusherCollection.findOneAndUpdate(filter, Updates.combine(updatePredicates));
+
+            System.out.println("Updating the generator maintenence stock");
+            System.out.println(newVersion);
         }
 
     }
