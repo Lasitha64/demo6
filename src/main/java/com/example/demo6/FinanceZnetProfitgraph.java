0081ce1736a27e945d98a16ee8b04193aa49cc6d
Kisaja <87978775+Kisaja@users.noreply.github.com>
2021-09-18 19:43:33 +0530

    Done till N

---
+++ src/main/java/com/example/demo6/FinanceZnetProfitgraph.java
@@ -0,0 +1,25 @@
+package com.example.demo6;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class FinanceZnetProfitgraph {
+
+    @FXML
+    private Button Back;
+
+    @FXML
+    private Button Home;
+
+    @FXML
+    void go_to_a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void go_to_j(ActionEvent event) {
+
+    }
+
+}
