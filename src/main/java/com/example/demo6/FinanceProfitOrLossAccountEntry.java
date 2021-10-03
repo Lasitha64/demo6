60ebb3f35102eb348bdbb4573ea9f1885e3ec0af
Kisaja <87978775+Kisaja@users.noreply.github.com>
2021-10-03 13:06:46 +0530

    update and delete done need to modify

--- src/main/java/com/example/demo6/FinanceProfitOrLossAccountEntry.java
+++ src/main/java/com/example/demo6/FinanceProfitOrLossAccountEntry.java
@@ -85,6 +85,39 @@ public class FinanceProfitOrLossAccountEntry {
 
     @FXML
     void Updatedb(ActionEvent event) {
+        // update one document
+        String POL_ID = ID.getText();
+        double SalesText = Double.parseDouble(Sales.getText());
+        double Cost_of_salesText = Double.parseDouble(Credit.getText());
+        double other_incomeText = Double.parseDouble(other_income.getText());
+        double other_expensesText = Double.parseDouble(other_expenses.getText());
+
+        System.out.println(POL_ID + SalesText + Cost_of_salesText + other_incomeText + other_expensesText);
+
+        Bson filter = eq("entry ID", POL_ID);
+
+
+        Bson updateSales = set("Sales", SalesText); // creating an array with a comment.
+        Bson updateCost_of_sales = set("Cost_of_sales", Cost_of_salesText); // using addToSet so no effect.
+        Bson updateother_income = set("other_income", other_incomeText); // using addToSet so no effect.
+        Bson updateother_expenses = set("other_expenses", other_expensesText); // using addToSet so no effect.
+
+        List<Bson> updatePredicates = new ArrayList<Bson>();
+        updatePredicates.add(updateSales);
+        updatePredicates.add(updateCost_of_sales);
+        updatePredicates.add(updateother_income);
+        updatePredicates.add(updateother_expenses);
+
+
+        //Bson updateOperation = set("Name", NameText);
+        /*.append("Name", NameText)
+                .append("Quantity", QuantityText)
+                .append("Prise", PriseText);*/
+        FindOneAndUpdateOptions optionAfter = new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER);
+        Document newVersion = POLCollection.findOneAndUpdate(filter, Updates.combine(updatePredicates));
+
+        System.out.println("Updating the Profit or loss account");
+        System.out.println(newVersion);
 
     }
 
@@ -110,6 +143,10 @@ public class FinanceProfitOrLossAccountEntry {
 
     @FXML
     void deletedb(ActionEvent event) {
+        String POL_ID = ID.getText();
+        Bson filter = eq("entry ID", POL_ID);
+        DeleteResult result = POLCollection.deleteOne(filter);
+        System.out.println(result);
 
     }
 
