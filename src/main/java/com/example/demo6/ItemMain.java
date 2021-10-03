b9f739556ec9cb3031ce973cd8ff67ffa6d8dc16
Hansaka2000 <hansakalakshan2000@gmail.com>
2021-10-03 15:24:12 +0530

    update

--- src/main/java/com/example/demo6/ItemMain.java
+++ src/main/java/com/example/demo6/ItemMain.java
@@ -7,16 +7,24 @@ import com.mongodb.client.MongoDatabase;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import org.bson.Document;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,14 +33,14 @@ public class ItemMain implements Initializable {
     private String itid;
     private String itname;
     private String itqunt;
+    private String itprice;
+    private String itdate;
+    private String itdes;
     MongoClient database;
     MongoCollection<Document> ItemCollection;
 
     @FXML
     private TextField search_fld;
-
-    @FXML
-    private Button buttn_search;
 
     @FXML
     private TableView<Item> StockItem;
@@ -59,10 +67,16 @@ public class ItemMain implements Initializable {
     private Button buutn_back;
 
     public ObservableList<Item> list;
+    public ObservableList<Item> searchlist;
 
     @FXML
-    void Back(ActionEvent event) {
-
+    void Back(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("stcock-mngt.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("stylesheet/stock-mngt.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
@@ -71,19 +85,20 @@ public class ItemMain implements Initializable {
     }
 
     @FXML
-    void Search(ActionEvent event) {
-
-    }
-
-    @FXML
-    void Update(ActionEvent event) {
-
+    void Update(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("stock-view.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("stylesheet/stock-view.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
         showItem();
+        searchitem();
     }
 
     public void showItem() {
@@ -91,7 +106,7 @@ public class ItemMain implements Initializable {
         ObservableList<Item> list = getItemList();
 
         item_id.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getId()));
-        item_name.setCellValueFactory(new PropertyValueFactory<Item, String>("Item_Name"));
+        item_name.setCellValueFactory(new PropertyValueFactory<Item, String>("Name"));
         item_qunt.setCellValueFactory(new PropertyValueFactory<Item, String>("Quantity"));
 
         StockItem.setItems(list);
@@ -118,7 +133,7 @@ public class ItemMain implements Initializable {
                 itname = doc.getString("Item_Name");
                 itqunt = doc.getString("Quantity");
 
-                attend.add(new Item(itid, itname, itqunt));
+                attend.add(new Item(itid, itname, itqunt, itprice, itdate, itdes));
 
             }
             //  list = FXCollections.observableArrayList(attend);
@@ -128,7 +143,42 @@ public class ItemMain implements Initializable {
         }
         return  attend;
 //      call the setTable method
+    }
 
+
+    @FXML
+    void searchitem() {
+
+        ObservableList<Item>  searchlist = getItemList();
+
+        item_id.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getId()));
+        item_name.setCellValueFactory(new PropertyValueFactory<Item, String>("Name"));
+        item_qunt.setCellValueFactory(new PropertyValueFactory<Item, String>("Quantity"));
+
+        StockItem.setItems(searchlist);
+
+        FilteredList<Item> filterdata = new FilteredList<>(searchlist, b->true);
+        search_fld.textProperty().addListener((observable, oldValue, newValue)->{
+            filterdata.setPredicate(item -> {
+                if(newValue == null || newValue.isEmpty()){
+                    return true;
+                }
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if(item.getId().toString().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                    return true;
+                }else if(item.getName().toLowerCase().indexOf(lowerCaseFilter)!= -1){
+                    return true;
+                }else if(item.getQuantity().toString().toLowerCase().indexOf(lowerCaseFilter) != -1){
+                    return true;
+                }else{
+                    return false;
+                }
+            });
+        });
+        SortedList<Item> sortedData = new SortedList<>(filterdata);
+        sortedData.comparatorProperty().bind(StockItem.comparatorProperty());
+        StockItem.setItems(sortedData);
 
     }
 
--- src/main/java/com/example/demo6/StockView.java
+++ src/main/java/com/example/demo6/StockView.java
@@ -2,22 +2,27 @@ package com.example.demo6;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.FindOneAndUpdateOptions;
 import com.mongodb.client.model.ReturnDocument;
 import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.DeleteResult;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -26,10 +31,19 @@ import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Updates.*;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class StockView {
+public class StockView implements Initializable {
+
+    private String itid;
+    private String itname;
+    private String itqunt;
+    private String itprice;
+    private String itdate;
+    private String itdes;
 
     @FXML
     private TextField search_fld;
@@ -37,14 +51,18 @@ public class StockView {
     @FXML
     private Button buttn_search;
 
-    @FXML
-    private TableColumn<?, ?> item_id;
 
     @FXML
-    private TableColumn<?, ?> item_name;
+    private TableView<Item> StockItem;
 
     @FXML
-    private TableColumn<?, ?> item_qunt;
+    private TableColumn<Item, String> item_id;
+
+    @FXML
+    private TableColumn<Item, String> item_name;
+
+    @FXML
+    private TableColumn<Item, String> item_qunt;
 
     @FXML
     private TextField idfld;
@@ -78,18 +96,6 @@ public class StockView {
 
     MongoCollection<Document> ItemCollection;
     MongoCollection<Document> StockItemReportCollection;
-
-    @FXML
-
-    public void initialize(){
-        //initialize database connection
-        Database databaseController = new Database();
-        MongoDatabase database = databaseController.connectToDB("HerathCMD");
-
-        // get collection
-        ItemCollection = database.getCollection("Item");
-        StockItemReportCollection = database.getCollection("StockItemReport");
-    }
 
     @FXML
     void Back(ActionEvent event) throws IOException {
@@ -161,6 +167,101 @@ public class StockView {
             System.out.println(newVersion);
             ab.display("Success","Data Updated Successfully!");
         }
+    }
+
+    public void showItem() {
+
+        ObservableList<Item> list = getItemList();
+
+        item_id.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getId()));
+        item_name.setCellValueFactory(new PropertyValueFactory<Item, String>("Name"));
+        item_qunt.setCellValueFactory(new PropertyValueFactory<Item, String>("Quantity"));
+
+        StockItem.setItems(list);
+
+    }
+
+    private ObservableList<Item> getItemList() {
+        ObservableList<Item> attend = FXCollections.observableArrayList();
+
+        MongoCursor<Document> cursor = ItemCollection.find().iterator();
+        try {
+
+            for (int i = 0; i < ItemCollection.count(); i++) {
+
+
+                Document doc = cursor.next();
+                itid = doc.getString("Item_ID");
+                //  System.out.println(itid);
+                itname = doc.getString("Item_Name");
+                itqunt = doc.getString("Quantity");
+
+                attend.add(new Item(itid, itname, itqunt, itprice, itdate, itdes));
+
+            }
+            //  list = FXCollections.observableArrayList(attend);
+        } finally {
+//          close the connection
+            cursor.close();
+        }
+        return  attend;
+//      call the setTable method
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //initialize database connection
+        Database databaseController = new Database();
+        MongoDatabase database = databaseController.connectToDB("HerathCMD");
+
+        // get collection
+        ItemCollection = database.getCollection("Item");
+
+        showItem();
+        searchitem();
+    }
+
+    @FXML
+    void searchitem() {
+
+        ObservableList<Item>  searchlist = getItemList();
+
+        item_id.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getId()));
+        item_name.setCellValueFactory(new PropertyValueFactory<Item, String>("Name"));
+        item_qunt.setCellValueFactory(new PropertyValueFactory<Item, String>("Quantity"));
+
+        StockItem.setItems(searchlist);
+
+        FilteredList<Item> filterdata = new FilteredList<>(searchlist, b->true);
+        search_fld.textProperty().addListener((observable, oldValue, newValue)->{
+            filterdata.setPredicate(item -> {
+                if(newValue == null || newValue.isEmpty()){
+                    return true;
+                }
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if(item.getId().toString().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                    return true;
+                }else if(item.getName().toLowerCase().indexOf(lowerCaseFilter)!= -1){
+                    return true;
+                }else if(item.getQuantity().toString().toLowerCase().indexOf(lowerCaseFilter) != -1){
+                    return true;
+                }else{
+                    return false;
+                }
+            });
+        });
+        SortedList<Item> sortedData = new SortedList<>(filterdata);
+        sortedData.comparatorProperty().bind(StockItem.comparatorProperty());
+        StockItem.setItems(sortedData);
+
+    }
+
+    @FXML
+    void handleMouseAction(MouseEvent event) {
+        Item item = StockItem.getSelectionModel().getSelectedItem();
+        idfld.setText(item.getId());
+        namefld.setText(item.getName());
     }
 
 }
